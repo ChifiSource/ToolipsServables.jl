0d94770889772d3f5277aa84fba72e8d2659cb04
emmy <52672675+emmaccode@users.noreply.github.com>
2025-01-02 00:35:10 -0700

    reimplemented `caret` position binding

--- src/ToolipsServables.jl
+++ src/ToolipsServables.jl
@@ -597,7 +597,7 @@ function string(anim::KeyFrames)
 end
 
 function show(io::Base.TTY, c::AbstractComponent)
-    show(io, MIME"text/markdown"(), string(mdcomponent(c)))
+    show(io, MIME"text/plain"(), c.name * join(string(prop) for prop in c.properties))
 end
 
 function show(io::Base.TTY, c::StyleComponent)
--- src/templating.jl
+++ src/templating.jl
@@ -382,6 +382,72 @@ function textdiv(name::String, p::Pair{String, <:Any} ...; text::String = "",
     return(box)::Component{:div}
 end
 
+function textdiv_caret_tracker!(comp::Component{:div})
+    name = comp.name
+    caretpos = script("caretposition", text = """
+    function getCaretIndex$(name)(element) {
+  let position = 0;
+  const isSupported = typeof window.getSelection !== "undefined";
+  if (isSupported) {
+    const selection = window.getSelection();
+    if (selection.rangeCount !== 0) {
+      const range = window.getSelection().getRangeAt(0);
+      const preCaretRange = range.cloneRange();
+      preCaretRange.selectNodeContents(element);
+      preCaretRange.setEnd(range.endContainer, range.endOffset);
+      position = preCaretRange.toString().length;
+    }
+  }
+  document.getElementById('$name').setAttribute('caret',position);
+}
+function createRange(node, chars, range) {
+    if (!range) {
+        range = document.createRange()
+        range.selectNode(node);
+        range.setStart(node, 0);
+    }
+
+    if (chars.count === 0) {
+        range.setEnd(node, chars.count);
+    } else if (node && chars.count >0) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            if (node.textContent.length < chars.count) {
+                chars.count -= node.textContent.length;
+            } else {
+                 range.setEnd(node, chars.count);
+                 chars.count = 0;
+            }
+        } else {
+            for (var lp = 0; lp < node.childNodes.length; lp++) {
+                range = createRange(node.childNodes[lp], chars, range);
+
+                if (chars.count === 0) {
+                   break;
+                }
+            }
+        }
+   }
+
+   return range;
+};
+
+function setCurrentCursorPosition$(name)(chars) {
+    chars = chars + 3;
+    if (chars >= 0) {
+        var selection = window.getSelection();
+
+        range = createRange(document.getElementById("$(name)").parentNode, { count: chars });
+
+        if (range) {
+            range.collapse(false);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+};""")
+    push!(comp[:extras], caretpos)
+    comp[:oninput] = comp[:oninput] * ";getCaretIndex$(name)(this);"
+end
 """
 ```julia
 textbox(name::String, range::UnitRange = 1:10, args::Pair{String, <:Any} ...; text::String = "", 
